09f5f4e0f9f11a2c634b9b1110de2c43fbb9be90
Ben Elferink <ben.elferink@icloud.com>
2020-12-29 21:12:00 +0200

    tweaked Sidebar UI

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SidebarOption from '../SidebarOption/SidebarOption';
 import { Button } from '@material-ui/core';
 import AddRoundedIcon from '@material-ui/icons/Add';
@@ -7,20 +7,36 @@ import StarRoundedIcon from '@material-ui/icons/Star';
 import SendRoundedIcon from '@material-ui/icons/Send';
 import NoteRoundedIcon from '@material-ui/icons/Note';
 import DeleteRoundedIcon from '@material-ui/icons/Delete';
+import ExpandMoreRoundedIcon from '@material-ui/icons/ExpandMoreRounded';
 import styles from './style/Sidebar.module.css';
 
 function Sidebar() {
+  const [showMore, setShowMore] = useState(false);
+  const toggleShowMore = () => setShowMore(!showMore);
+
   return (
     <div className={styles.sidebar}>
       <Button className={styles.sidebar__compose} startIcon={<AddRoundedIcon fontSize='large' />}>
         Compose
       </Button>
 
-      <SidebarOption Icon={InboxRoundedIcon} title='Inbox' number={54} />
-      <SidebarOption Icon={StarRoundedIcon} title='Starred' number={54} />
-      <SidebarOption Icon={SendRoundedIcon} title='Sent' number={54} />
-      <SidebarOption Icon={NoteRoundedIcon} title='Drafts' number={54} />
-      <SidebarOption Icon={DeleteRoundedIcon} title='Trash' number={54} />
+      <SidebarOption Icon={InboxRoundedIcon} title='Inbox' number={42} />
+      <SidebarOption Icon={StarRoundedIcon} title='Starred' number={42} />
+      <SidebarOption Icon={SendRoundedIcon} title='Sent' number={42} selected={true} />
+      <SidebarOption Icon={NoteRoundedIcon} title='Drafts' number={42} />
+
+      <SidebarOption
+        Icon={ExpandMoreRoundedIcon}
+        title='More'
+        number={1}
+        onClick={toggleShowMore}
+        className={showMore ? styles.showMore__on : styles.showMore__off}
+      />
+      {showMore && (
+        <>
+          <SidebarOption Icon={DeleteRoundedIcon} title='Trash' number={42} />
+        </>
+      )}
     </div>
   );
 }
--- client/src/components/SidebarOption/SidebarOption.jsx
+++ client/src/components/SidebarOption/SidebarOption.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './style/SidebarOption.module.css';
 
-function SidebarOption({ Icon, title, number, selected }) {
+function SidebarOption({ Icon, title, number, selected, onClick, className }) {
   return (
-    <div className={`${styles.sidebarOption} ${selected && styles.sidebarOption__active}`}>
+    <div
+      className={`${styles.sidebarOption} ${selected && styles.active} ${className}`}
+      onClick={onClick}>
       <Icon fontSize='large' />
       <h3>{title}</h3>
       <p>{number}</p>

10aab201f9c47190dc731f994b7c1f827b804cc2
Ben Elferink <ben.elferink@icloud.com>
2020-12-31 04:08:43 +0200

    post /register validation

---
+++ server/api/middleware/validateUser.js
@@ -0,0 +1,21 @@
+import { check } from 'express-validator';
+import User from './../models/User.js';
+
+export const registerValidations = [
+  check('email', 'Email is not valid')
+    .exists()
+    .isEmail()
+    .normalizeEmail()
+    .custom((value) =>
+      User.findOne({ email: value }).then((user) =>
+        !user ? true : Promise.reject('That email is already taken'),
+      ),
+    ),
+  check('password', 'Password must be over 7 characters').exists().isLength({ min: 7 }),
+  check('passwordConfirm')
+    .exists()
+    .custom((value, { req }) =>
+      value === req.body.password ? true : Promise.reject('Passwords do not match'),
+    ),
+  check(['firstName', 'middleName', 'lastName'], 'Name is not valid').matches(/^[a-z ,.'-]+$/i),
+];

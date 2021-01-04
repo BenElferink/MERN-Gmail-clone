2d025e99336767dec510e9fe7cec52ff077eb102
Ben Elferink <ben.elferink@icloud.com>
2021-01-04 19:09:31 +0200

    fixes and improvments

--- client/src/components/EmailView/EmailView.jsx
+++ client/src/components/EmailView/EmailView.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import EmailOptions from '../EmailOptions/EmailOptions';
-import { Avatar, Checkbox, IconButton } from '@material-ui/core';
-import RefreshRoundedIcon from '@material-ui/icons/RefreshRounded';
-import MoreVertRoundedIcon from '@material-ui/icons/MoreVertRounded';
+import { IconButton, Avatar } from '@material-ui/core';
+import ArrowBackRoundedIcon from '@material-ui/icons/ArrowBackRounded';
 import styles from './style/EmailView.module.css';
 
 function EmailView() {
+  const history = useHistory();
   const { id } = useParams();
+
   const mailbox = useSelector((state) => state.user.mailbox);
+
   const [mailToDisplay] = useState(() => {
     for (let i = 0; i < mailbox.length; i++) {
       const mailId = mailbox[i]._id;
@@ -20,17 +22,12 @@ function EmailView() {
   return (
     <div className={styles.container}>
       <EmailOptions>
-        <Checkbox />
-        <IconButton onClick={() => dispatch(getUserData(token))}>
-          <RefreshRoundedIcon />
-        </IconButton>
-        <IconButton>
-          <MoreVertRoundedIcon />
+        <IconButton onClick={() => history.goBack()}>
+          <ArrowBackRoundedIcon />
         </IconButton>
       </EmailOptions>
 
       <div>
-        {mailToDisplay._id}
         <h3>{mailToDisplay.subject}</h3>
         <div>
           <Avatar />
